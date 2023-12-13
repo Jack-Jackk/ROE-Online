8476af118f2857700cda40c97e3b3a3daa050c53
Jack-Jackk <66342920+Jack-Jackk@users.noreply.github.com>
2023-12-12 20:20:01 -0500

    created new scene, added world border, and camera limit

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -7,7 +7,7 @@ var player_state
 
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
     var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
     if direction.x == 0 and direction.y ==0: 
